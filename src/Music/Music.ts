a7734b32456318158eeb2d981315dbd686ea2f59
naseif <d2mds21@gmail.com>
2021-12-15 13:33:34 +0100

    Some new changes and more music services support

--- src/Music/Music.ts
+++ src/Music/Music.ts
@@ -1,4 +1,18 @@
-import { yt_validate, search, playlist_info } from 'play-dl';
+import {
+  yt_validate,
+  search,
+  playlist_info,
+  deezer,
+  spotify,
+  sp_validate,
+  dz_validate,
+  Spotify,
+  DeezerAlbum,
+  DeezerPlaylist,
+  SoundCloud,
+  so_validate,
+  soundcloud
+} from 'play-dl';
 import { Track } from './Track';
 import { Playlist } from './Playlist';
 
@@ -6,7 +20,7 @@ export class Music {
   /**
    * Searches for a track or a playlist on youtube
    * @param {string} query: string | url
-   * @returns Track | Playlist
+   * @returns Promise<Track[] | Playlist[]>
    */
 
   async youtube(query: string): Promise<Track[] | Playlist[]> {
@@ -35,7 +49,7 @@ export class Music {
         break;
       case 'search':
         result = await search(query);
-        if (!search) throw new Error('No Song was found for this query!');
+        if (!result) throw new Error('No Song was found for this query!');
         tracks = result.map((track: any) => {
           console.log(track.thumbnails[0].url);
           return new Track(track);
@@ -44,5 +58,70 @@ export class Music {
     }
 
     return tracks;
+  }
+
+  /**
+   *  Searches for a track, playlist or album on deezer
+   * @param {string} query
+   * @returns Promise<Track | Playlist | DeezerAlbum | DeezerPlaylist>
+   */
+  async deezer(
+    query: string
+  ): Promise<Track | Playlist | DeezerAlbum | DeezerPlaylist> {
+    if (!query) throw new Error('No search query was provided!');
+    const validate = await dz_validate(query);
+    if (!validate) throw new Error('This is not a valid search query!');
+    let tracks;
+    let result: any;
+    switch (validate) {
+      case 'search':
+        result = await search(query, { source: { deezer: 'track' } });
+        tracks = result.map((track: any) => {
+          return new Track(track);
+        });
+        break;
+      case 'playlist':
+        result = await deezer(query);
+        tracks = result;
+        break;
+      case 'track':
+        result = await search(query, { source: { deezer: 'track' } });
+        tracks = result.map((track: any) => {
+          return new Track(track);
+        });
+        break;
+      case 'album':
+        result = await deezer(query);
+        tracks = result;
+        break;
+    }
+    return tracks;
+  }
+
+  /**
+   * Searches for a track, playlist or album on Spotify
+   * @param {string} query
+   * @returns Promise<Spotify>
+   */
+
+  async spotify(query: string): Promise<Spotify> {
+    if (!query) throw new Error('No search query was provided!');
+    const validate = sp_validate(query);
+    if (!validate) throw new Error('This is not a valid search query!');
+
+    return await spotify(query);
+  }
+
+  /**
+   * Gets a track or a playlist from Soundcloud
+   * @param {string} query
+   * @returns Promise<SoundCloud>
+   */
+  async soundcloud(query: string): Promise<SoundCloud> {
+    if (!query) throw new Error('No search query was provided!');
+    const validate = so_validate(query);
+    if (!validate) throw new Error('This is not a valid search query!');
+
+    return await soundcloud(query);
   }
 }
--- src/Music/Track.ts
+++ src/Music/Track.ts
@@ -2,25 +2,37 @@ export interface TrackData {
   durationInSec: number;
   title: string;
   url: string;
-  thumbnails: any[];
+  thumbnails?: any[];
   duration: string;
   type: string;
-  description: string;
-  views: number;
-  channel: { name: string; url: string };
-  private: boolean;
+  description?: string;
+  views?: number;
+  channel?: { name: string; url: string };
+  private?: boolean;
+  shortTitle?: string;
+  rank?: number;
+  explicit?: string;
+  previewURL?: string;
+  artist?: { id: number; name: string; url: string; picture: {} };
+  album?: {};
 }
 
 export class Track {
   title: string;
   url: string;
-  thumbnails: any[];
+  thumbnails?: any[];
   duration: number;
   type: string;
-  description: string;
-  views: number;
-  channel: {};
-  private: boolean;
+  description?: string;
+  views?: number;
+  channel?: {};
+  private?: boolean;
+  shortTitle?: string;
+  rank?: number;
+  explicit?: string;
+  previewURL?: string;
+  artist?: { id: number; name: string; url: string; picture: {} };
+  album?: {};
 
   constructor(track: TrackData) {
     if (!track) throw new Error('Constructor was not initialized properly!');
@@ -32,9 +44,15 @@ export class Track {
     this.description = track?.description;
     this.views = track?.views;
     this.channel = {
-      name: track.channel.name,
-      url: track.channel.url
+      name: track?.channel?.name,
+      url: track?.channel?.url
     };
     this.private = track?.private;
+    this.shortTitle = track?.shortTitle;
+    this.rank = track?.rank;
+    this.explicit = track?.explicit;
+    this.previewURL = track?.previewURL;
+    this.artist = track?.artist;
+    this.album = track?.album;
   }
 }
